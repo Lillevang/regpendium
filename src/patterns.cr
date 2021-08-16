fd9eaa85c2dad0802ce4de22604739529a41bbd3
Jeppe Lillevang Salling <jeppe.lillevang@gmail.com>
2021-08-16 23:21:47 +0200

    Changed ipv4 init to use helper function

--- src/patterns.cr
+++ src/patterns.cr
@@ -3,6 +3,12 @@ class Patterns
     getter ip_v4 : Regex
 
     def initialize()
-      @ip_v4 = /^([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))$/
+      @ip_v4 = setup_ipv4()
     end
+
+    def setup_ipv4() : Regex
+      sub_pattern = "([01]?\\d{1,2}|2(5[0-5]|[0-4]\\d))"
+      Regex.new("^#{sub_pattern}\\.#{sub_pattern}\\.#{sub_pattern}\\.#{sub_pattern}$")
+    end
+
 end
