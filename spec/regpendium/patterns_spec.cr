6ff240827e9afc429ee74fe85c06e080563e9ccd
Lillevang <jeppe.lillevang@gmail.com>
2021-08-25 00:41:24 +0200

    More structure to the project

--- spec/regpendium/patterns_spec.cr
+++ spec/regpendium/patterns_spec.cr
@@ -9,15 +9,15 @@ module Regpendium
 
     it "contains an ip_v4 pattern" do
       patterns = Patterns.new
-      patterns.ip_v4.should_not be nil
-      patterns.ip_v4.should eq /^([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))$/
+      patterns.ipv4.should_not be nil
+      patterns.ipv4.should eq /^([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))\.([01]?\d{1,2}|2(5[0-5]|[0-4]\d))$/
     end
 
     it "matches ip_v4 addresses as expected" do
       patterns = Patterns.new
       ip_v4_data = create_ip_v4_data()
       ip_v4_data.each do |ip, exists|
-        patterns.ip_v4.matches?(ip).should eq exists
+        patterns.ipv4.matches?(ip).should eq exists
       end
     end
   end
--- src/regpendium/patterns.cr
+++ src/regpendium/patterns.cr
@@ -1,14 +1,33 @@
 module Regpendium
   class Patterns
-    getter ip_v4 : Regex
 
     def initialize
-      @ip_v4 = setup_ipv4
     end
 
-    def setup_ipv4 : Regex
-      sub_pattern = "([01]?\\d{1,2}|2(5[0-5]|[0-4]\\d))"
-      Regex.new("^#{sub_pattern}\\.#{sub_pattern}\\.#{sub_pattern}\\.#{sub_pattern}$")
+    def ipv4 : Regex
+      IPV4
+    end
+
+    # TODO
+    def zip_code(country_code : String) : Regex
+      Regex.new(ZIP_CODE[country_code])
+    end
+
+
+    # Returns a tuple with two patterns to match credit card numbers.
+    # This will accept patterns like:
+    # Index 0 will accept patterns like: XXXXXXXXXXXXXXXX, XXXX-XXXX XXXX-XXXX, XXXX XXXX-XXXX XXXX,
+    # Index 1 will accept patterns like: XXXXXXXXXXXX, XXXX XXXX XXXX XXXX, or XXXX-XXXX-XXXX-XXXX (stricter)
+    def credit_card() : Tuple(Regex, Regex)
+      {/\b(\d{4}[-\s]?){3}\d{4}\b/, /\b((\d{4}){3}|(\d{4}-){3}|(\d{4}\s){3})\d{4}\b/}
+    end
+
+    def email():
+      /([A-Za-z0-9!#$%&'*+\/=?^_`{|.}~-]+@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?)/
+    end
+
+    def hex_value():
+      /\b#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\b/
     end
   end
 end
